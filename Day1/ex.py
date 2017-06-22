ba78c14609b0477d9e89b2a28cac77ee63eb3db2
mclaughlin6464 <mclaughlin6464@gmail.com>
2017-06-22 16:32:34 -0700

    Satisfied with Day 1

---
+++ Day1/ex.py
@@ -0,0 +1,9 @@
+print "Hello from the global scope!"
+
+def exFunc():
+    print "Hello from inside exFunc"
+
+if __name__ == '__main__':
+    # This will only run if you run `python ex.py`
+    x = 2+ 2
+    print x
